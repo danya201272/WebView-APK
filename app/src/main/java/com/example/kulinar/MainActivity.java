56871999ae615ee9a9049fc0138366bf81b90497
danya201272 <36302863+danya201272@users.noreply.github.com>
2023-12-05 06:58:45 +0300

    Update

--- app/src/main/java/com/example/kulinar/MainActivity.java
+++ app/src/main/java/com/example/kulinar/MainActivity.java
@@ -1,37 +1,39 @@
 package com.example.kulinar;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.net.http.SslError;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import android.webkit.CookieManager;
 import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 public class MainActivity extends AppCompatActivity {
-
     WebView webView;
     SwipeRefreshLayout swipeRefreshLayout;
-
     String url = "https://m.povar.ru/";
+    boolean loadingFinished = true;
+    boolean redirect = false;
 
-    @SuppressLint("SetJavaScriptEnabled")
+    @SuppressLint({"SetJavaScriptEnabled"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         CookieManager.getInstance().setAcceptCookie(true);
-        this.getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
         webView = findViewById(R.id.web);
         swipeRefreshLayout = findViewById(R.id.swipe);
         webView.getSettings().setJavaScriptEnabled(true);
@@ -39,36 +41,75 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setDomStorageEnabled(true);
         webView.setWebViewClient(new myWebViewclient());
         webView.loadUrl(url);
+        fullscreenons();
 
         swipeRefreshLayout.setOnRefreshListener(() -> {
             swipeRefreshLayout.setRefreshing(true);
             new Handler().postDelayed(() -> {
                 swipeRefreshLayout.setRefreshing(false);
-                webView.loadUrl(url);
+                webView.reload();
+                fullscreenons();
             },  3000);
         });
     }
+    public void fullscreenons() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            final WindowInsetsController controller = getWindow().getInsetsController();
+            if (controller != null)
+                controller.hide(WindowInsets.Type.statusBars());
+        }
+        else {
+            getWindow().getDecorView().setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_IMMERSIVE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        }
+    }
 
-    public static class myWebViewclient extends WebViewClient{
+    public class myWebViewclient extends WebViewClient{
         @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
             CookieManager.getInstance().flush();
-            return false;
+            if (!loadingFinished) {
+                redirect = true;
+            }
+            loadingFinished = false;
+            return true;
         }
         @Override
         public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
             super.onReceivedSslError(view, handler, error);
             handler.cancel();
         }
+        @Override
+        public void onPageStarted(
+                WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            loadingFinished = false;
+            fullscreenons();
+            //SHOW LOADING IF IT ISNT ALREADY VISIBLE
+        }
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            if (!redirect) {
+                loadingFinished = true;
+                fullscreenons();
+                //HIDE LOADING IT HAS FINISHED
+            } else {
+                redirect = false;
+            }
+        }
     }
-
-
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
 
         if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
             webView.goBack();
+            fullscreenons();
             return true;
         }
         return super.onKeyDown(keyCode, event);
